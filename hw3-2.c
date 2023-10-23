8506bf556c7c95210f326db1a9efd3edd47f856c
HojunXie <hojun.xie2@gmail.com>
2023-10-23 23:07:53 +0800

    Fix hw3-2 logic

--- hw3-2.c
+++ hw3-2.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+int inBetween(int, int, int);
+
 int main() {
   int input[6];
   for (int i = 0; i < 6; i++) {
@@ -14,36 +16,36 @@ int main() {
       return 1;
   }
 
-  int output = 1;
-  int ab = 0;
-  int ac = 0;
-  int bc = 0;
+  int output;
 
-  if ((input[0] > input[2] && input[0] < input[3]) || (input[1] > input[2] && input[1] < input[3])) {
-    ab++;
-    output++;
-  } else if ((input[0] > input[4] && input[0] < input[5]) || (input[1] > input[4] && input[1] < input[5])) {
-    ac++;
-    output++;
+  if (inBetween(input[0], input[2], input[3]) || inBetween(input[1], input[2], input[3]) || inBetween(input[2], input[0], input[1]) || inBetween(input[3], input[0], input[1])) { //ab
+    if (inBetween(input[0], input[4], input[5]) || inBetween(input[1], input[4], input[5]) || inBetween(input[4], input[0], input[1]) || inBetween(input[5], input[0], input[1])) { //ac
+      if (inBetween(input[2], input[4], input[5]) || inBetween(input[3], input[4], input[5]) || inBetween(input[4], input[2], input[3]) || inBetween(input[5], input[2], input[3])) { //bc
+        output = 3;
+      } else {
+        output = 2;
+      }
+    } else {
+      output = 2;
+    }
+  } else if (inBetween(input[2], input[4], input[5]) || inBetween(input[3], input[4], input[5]) || inBetween(input[4], input[2], input[3]) || inBetween(input[5], input[2], input[3])) { //bc
+    if (inBetween(input[0], input[4], input[5]) || inBetween(input[1], input[4], input[5]) || inBetween(input[4], input[0], input[1]) || inBetween(input[5], input[0], input[1])) { //ac
+     output = 3;
+    } else {
+      output = 2;
+    }
+  } else {
+    output = 1;
   }
 
-  if ((input[2] > input[0] && input[2] < input[1] && ab == 0) || (input[3] > input[0] && input[3] < input[1] && ab == 0)) {
-    ab++;
-    output++;
-  } else if ((input[2] > input[4] && input[2] < input[5]) || (input[3] > input[4] && input[3] < input[5])) {
-    bc++;
-    output++;
-  }
-
-  if ((input[4] > input[2] && input[4] < input[3] && bc == 0 && output != 3) || (input[5] > input[2] && input[5] < input[3] && bc == 0 && output != 3)) {
-    bc++;
-    output++;
-  } else if ((input[4] > input[0] && input[4] < input[1] && ac == 0 && output != 3) || (input[5] > input[0] && input[5] < input[1] && ac == 0 && output != 3)) {
-    ac++;
-    output++;
-  }
-  
   printf("%d", output);
 
+  return 0;
+}
+
+int inBetween(int a, int b, int c) {
+  if (a > b && a < c) {
+    return 1;
+  }
   return 0;
 }
